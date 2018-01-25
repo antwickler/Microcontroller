e04a59f1b88564c11983a611c0bd3f1cc884a983
ANTWICKLER <antwickler@users.noreply.github.com>
2018-01-25 18:21:16 +0700

    Update Program03.asm

--- Program03.asm
+++ Program03.asm
@@ -1,14 +1,18 @@
 /*
- *  AVR Microcontroller ATmega328P
- *  Keypad.asm
- *
- *  Created: 3/2/2017 12:21:02 PM
- *  Author: Antwickler
+ *  Write assembly language, which is the equivalent of AVR function of C language the following
+ *  
+ *  unsigned char a=20, b=32, c=70, d=3, e;
+ *  c = (a+b) - (c-d);
+ *  e = c-10;		
+ *  b++; 
+ *  d = (c+b) - e;		
+ *  a--;
+ *  
  */ 
 
 .include "m328Pdef.inc"
 
-.DEF  VAR_A = R16
+.DEF	VAR_A = R16
 .DEF	VAR_B = R17
 .DEF	VAR_C = R18
 .DEF	VAR_D = R19
@@ -21,4 +25,24 @@
 .ORG	0x00
 	rjmp	RESET
 
-RESET:	
+RESET:	ldi	VAR_A, 20
+	ldi	VAR_B, 32
+	ldi	VAR_C, 70
+	ldi	VAR_D, 3
+	mov	VAR_F, VAR_C
+	sub	VAR_F, VAR_D
+	mov	VAR_H, VAR_A
+	add	VAR_H, VAR_B
+	mov	VAR_C, VAR_H
+	sub	VAR_C, VAR_F
+	mov	VAR_E, VAR_C
+	subi	VAR_E, 10
+	inc	VAR_B
+	mov	VAR_G, VAR_C
+	sub	VAR_D, VAR_E
+	dec	VAR_A
+	
+END:	rjmp	END
+
+.DSEG
+.ESEG
